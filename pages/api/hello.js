f05824125fe9ed6962b6e031d177127d7f75b1d6
pifragile <pg@solid-bit.com>
2024-07-25 14:28:10 +0800

    remove db

--- pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-import db from "../../lib/db";
-
-export default async function handler(req, res) {
-    let abc = await db.getAll();
-    res.status(200).json({ text: "Hello" + process.env.VAR0 + abc[0].eis });
-}
