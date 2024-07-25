6424ddb739fea7af332283bb7b3a0c5fafe28f22
pifragile <pg@solid-bit.com>
2024-07-25 14:22:07 +0800

    init

---
+++ pages/api/artworks.js
@@ -0,0 +1,23 @@
+import parser from "xml2json";
+
+export default async function handler(req, res) {
+    const baseUrl = "https://systema.fra1.digitaloceanspaces.com/";
+
+    const response = await fetch(baseUrl);
+    const listing = parser.toJson(await response.text(), { object: true });
+
+    let keys = listing.ListBucketResult.Contents;
+    keys = keys
+        .filter((e) => e.Key.includes("."))
+        .reduce((acc, cur) => {
+            let projectName = cur.Key.split("/")[0];
+            acc[projectName] = acc[projectName] || [];
+            cur.url = baseUrl + cur.Key
+            acc[projectName].push(cur);
+            return acc;
+        }, {});
+
+    
+    console.log(keys)
+    res.status(200).json(keys);
+}
